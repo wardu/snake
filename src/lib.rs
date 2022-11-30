a8d42359b4c45439b7e837be66ab16bd84ff98cf
Warren Dubery <warrendubery@protonmail.ch>
2022-11-30 16:53:45 +0000

    draw snake

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,37 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+struct SnakeCell(usize);
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![(SnakeCell(spawn_index))],
+        }
+    }
+}
 #[wasm_bindgen]
 pub struct World {
-    pub width: usize,
+    width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 10 }
+        World {
+            width: 10,
+            snake: Snake::new(10),
+        }
     }
     pub fn get_width(&self) -> usize {
         self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,7 +1,43 @@
 import init, { World } from "snake";
 
 init().then((_) => {
+  const CELL_SIZE = 14;
+
   const world = World.new();
+  const worldWidth = world.get_width();
+
   const canvas = document.getElementById("canvas");
   const context = canvas.getContext("2d");
+
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
+
+  const drawWorld = () => {
+    context.beginPath();
+
+    for (let i = 0; i <= worldWidth; i++) {
+      context.moveTo(i * CELL_SIZE, 0);
+      context.lineTo(i * CELL_SIZE, worldWidth * CELL_SIZE);
+    }
+    for (let j = 0; j <= worldWidth; j++) {
+      context.moveTo(0, j * CELL_SIZE);
+      context.lineTo(worldWidth * CELL_SIZE, j * CELL_SIZE);
+    }
+
+    context.stroke();
+  };
+
+  const drawSnake = () => {
+    const snakeIdx = world.snake_head_idx();
+    const col = snakeIdx % worldWidth;
+    const row = Math.floor(snakeIdx / worldWidth);
+
+    context.beginPath();
+    context.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+    context.stroke();
+  };
+
+  drawWorld();
+  drawSnake();
 });
